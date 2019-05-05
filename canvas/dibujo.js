4a7e653b3b35562f66f4a9415826300ce1fade40
Fedora <raziel_lucio@icloud.com>
2019-05-05 13:05:13 -0500

    Agregando boton a lienzo

--- canvas/dibujo.js
+++ canvas/dibujo.js
@@ -1,21 +1,11 @@
+var texto = document.getElementById("texto_lineas");
+var boton = document.getElementById("botoncito");
+boton.addEventListener("click",dibujoPorClick);
+
 var d = document.getElementById("dibujito");
+var ancho = d.width;
 var lienzo = d.getContext("2d");
-var lineas = 30;
-var l = 0;
-var yi, xf;
-var colorcito = "#FAA"
 
-for(l=0; l<lineas; l++){
-  yi = 10 * l;
-  xf = 10 * (l + 1);
-  dibujarLinea(colorcito, 0,yi,xf,300);
-  dibujarLinea(colorcito, yi,0,300,xf);
-}
-
-dibujarLinea("red",1,1,1,300);
-dibujarLinea("red",1,299,299,299);
-dibujarLinea("red",299,1,299,299);
-dibujarLinea("red",1,1,299,1);
 
 function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal){
   lienzo.beginPath();
@@ -24,4 +14,25 @@ function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal){
   lienzo.lineTo(xfinal,yfinal);
   lienzo.stroke();
   lienzo.closePath();
+}
+
+function dibujoPorClick(){
+
+  var lineas = parseInt(texto.value);
+  var l = 0;
+  var yi, xf;
+  var colorcito = "#FAA"
+  var espacio = ancho / lineas;
+
+  for(l=0; l<lineas; l=l+0.5){ //Operacion para dibujar el doble de lineas
+    yi = espacio * l;
+    xf = espacio * (l + 1);
+    dibujarLinea(colorcito, 0,yi,xf,300);
+    dibujarLinea(colorcito, yi,0,300,xf); //Funcion invertida
+  }
+
+  dibujarLinea("red",1,1,1,300);
+  dibujarLinea("red",1,299,299,299);
+  dibujarLinea("red",299,1,299,299);
+  dibujarLinea("red",1,1,299,1);
 }
