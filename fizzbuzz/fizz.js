32612175f8cb52732158229bf50dd696707718e5
Fedora <raziel_lucio@icloud.com>
2019-05-11 16:30:22 -0500

    Agragando programa Fizz Buzz

---
+++ fizzbuzz/fizz.js
@@ -0,0 +1,29 @@
+
+var numeros = 100;
+
+for(var i=1; i<=100; i++){
+  var divisible = false;
+  if(esDivisible(i,3)){
+    document.write ("Fizz ");
+    divisible = true;
+  }
+
+  if(esDivisible(i,5)){
+    document.write("Buzz ");
+    divisible = true;
+  }
+
+  if(!esDivisible(i,3) && !esDivisible(i,5)) {
+    document.write(i);
+  }
+  document.write("<br/>");
+}
+
+function esDivisible(num, divisor){
+  if(num % divisor == 0){
+    return true;
+  }
+  else{
+    return false;
+  }
+}
